99115c587c5ac5df4e6112f0a225286cf323ae51
Philip <philiplmorris@googlemail.com>
2014-08-13 08:48:24 +0000

    add bear image

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
   var send = require('send');
   var server = http.createServer();
   server.on('request', function(req, res){
-    send(req, req.url, {root: './'}).pipe(res);
+    send(req, req.url, {root: './drunken-bear/'}).pipe(res);
    });
    server.listen(30000);
    console.log('server started on port 30000');
